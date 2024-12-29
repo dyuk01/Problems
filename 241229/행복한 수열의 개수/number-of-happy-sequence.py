b970914c0edc17f0618c173b0ec501a1f2cf0874
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2024-12-29 18:31:47 +0900

    Title: 행복한 수열의 개수, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241229/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,44 @@
+# Check for consecutive duplicate numbers in a row
+def check_row(matrix, m, row, c1, c2):
+    dupNum = 1
+    prevNum = 0
+    for col in range(c1, c2 + 1):
+        num = matrix[row][col]
+        if prevNum == num:
+            dupNum += 1
+        prevNum = num
+
+    if dupNum >= m:
+        return True
+
+# Check for consecutive duplicate numbers in a column
+def check_col(matrix, m, col, r1, r2):
+    dupNum = 1
+    prevNum = 0
+    for row in range(r1, r2 + 1):
+        num = matrix[row][col]
+        if dupNum == num:
+            dupNum += 1
+
+    if dupNum >= m:
+        return True
+
+
+# Initialize
+n, m = map(int, input().split())
+matrix = []
+happy_array = 0
+
+# Matrix Input
+for _ in range(n):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+
+# Happy Array
+for i in range(n):
+    if check_row(matrix, m, i, 0, n-1):
+        happy_array += 1
+    if check_col(matrix, m, i, 0, n-1):
+        happy_array += 1
+
+print(happy_array)
