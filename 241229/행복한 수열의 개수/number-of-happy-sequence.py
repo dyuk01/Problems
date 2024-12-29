ec4e30d0aa173c2ae7c3f2a85c2b70da1d72dcd9
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2024-12-29 18:55:34 +0900

    Title: 행복한 수열의 개수, Time: 53ms, Memory: 17MB, Status: Passed - CodeTree

--- 241229/행복한 수열의 개수/number-of-happy-sequence.py
+++ 241229/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,30 +1,31 @@
 # Check for consecutive duplicate numbers in a row
-def check_row(matrix, m, row, c1, c2):
-    dupNum = 1
+def check_row(matrix, m, row, c1, n):
+    dupNum = 0
     prevNum = -1
-    for col in range(c1, c2 + 1):
+    for col in range(c1, n):
         num = matrix[row][col]
         if prevNum == num:
             dupNum += 1
-            if dupNum >= m:
-                return True
         else:
             dupNum = 1
         prevNum = num
+        if dupNum >= m:
+            return True
     return False
+    
 # Check for consecutive duplicate numbers in a column
-def check_col(matrix, m, col, r1, r2):
-    dupNum = 1
+def check_col(matrix, m, col, r1, n):
+    dupNum = 0
     prevNum = -1
-    for row in range(r1, r2 + 1):
+    for row in range(r1, n):
         num = matrix[row][col]
         if prevNum == num:
             dupNum += 1
-            if dupNum >= m:
-                return True
         else:
             dupNum = 1
         prevNum = num
+        if dupNum >= m:
+            return True
     return False
 
 
@@ -40,9 +41,9 @@ for _ in range(n):
 
 # Happy Array
 for i in range(n):
-    if check_row(matrix, m, i, 0, n-1):
+    if check_row(matrix, m, i, 0, n):
         happy_array += 1
-    if check_col(matrix, m, i, 0, n-1):
+    if check_col(matrix, m, i, 0, n):
         happy_array += 1
 
 print(happy_array)
