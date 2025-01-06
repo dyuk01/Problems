57392503e1fe676649a7098f13c9c9a991ad7e5d
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-01-06 23:56:24 +0900

    Title: 떨어지는 1자 블록, Time: 53ms, Memory: 16MB, Status: Passed - CodeTree

--- 250106/떨어지는 1자 블록/falling-horizontal-block.py
+++ 250106/떨어지는 1자 블록/falling-horizontal-block.py
@@ -5,9 +5,8 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 # Check if the block fits in the selected row.
 def is_valid(row):
     for col in range(k, k + m):
-        if grid[row][col] == 1:
+        if col >= n or grid[row][col] == 1:
             return False
-    
     return True
 
 # Fills empty space in a grid with a block.
@@ -15,11 +14,16 @@ def fill(row):
     for col in range(k, k + m):
         grid[row][col] = 1
 
-
-for i in range(n - 1, -1, -1):
+# Main algorithm.
+rowIdx = -1
+for i in range(n):
     if is_valid(i):
-        fill(i)
+        rowIdx = i
+    else:
         break
+
+if rowIdx >= 0:
+    fill(rowIdx)
 
 for row in grid:
     print(" ".join(map(str, row)))
