6945fc8353753cdbc96adbcb48e374bdde201646
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-01-18 06:03:04 +0900

    Title: DateTime to DateTime, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250118/DateTime to DateTime/datetime-to-datetime.py
+++ 250118/DateTime to DateTime/datetime-to-datetime.py
@@ -1,13 +1,12 @@
 a, b, c = map(int, input().split())
 
 # Write your code here!
-res = 0
+res, day, hr, m = 0, 0, 0, 0
 
-if a > 11:
-    res += (a - 11) * 24 * 60
-if b > 11:
-    res += (b - 11) * 60
-if c > 11:
-    res += (c - 11)
+day = (a - 11) * 60 * 24
+hr = (b - 11) * 60
+m = c - 11
+
+res = day + hr + m
 
 print(res)
