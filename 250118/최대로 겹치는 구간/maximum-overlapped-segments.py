3eb117237b36d39420df83ec9168effd1c64e5cc
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-01-18 06:30:23 +0900

    Title: 최대로 겹치는 구간, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250118/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 250118/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -10,11 +10,16 @@ for x1, x2 in segments:
     if x2 > max_x:
         max_x = x2
 
-# 0-index the segment numbers.
-# Then, mark overlap
 overlaps = [0 for _ in range(min_x, max_x + 1)]
+# 0-index the segment numbers.
 for x1, x2 in segments:
-    for i in range(x1 - min_x, x2 - min_x + 1):
+    if x1 < 0:
+        x1 += (min_x) * -1
+        x2 += (min_x) * -1
+    if min_x > 0:
+        x1 -= min_x
+        x2 -= min_x
+    for i in range(x1, x2 + 1):
         overlaps[i] += 1
 
 res = 0
