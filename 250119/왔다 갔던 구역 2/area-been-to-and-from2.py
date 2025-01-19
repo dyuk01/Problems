f481ab35b2d9256ffeb5bb7286dbf587f68be715
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-01-19 15:04:38 +0900

    Title: 왔다 갔던 구역 2, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250119/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250119/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,85 +1,58 @@
-# n = int(input())
-# x = []
-# dir = []
-# for _ in range(n):
-#     xi, di = input().split()
-#     x.append(int(xi))
-#     dir.append(di)
-
-# # Collect all movements in (x1, x2) format.
-# curr_pos = 0
-# movements = []
-# for pos, direct in zip(x, dir):
-#     if direct == "L":
-#         pos *= -1
-#         movements.append((curr_pos + pos, curr_pos))
-#     else:
-#         movements.append((curr_pos, curr_pos + pos))
-#     curr_pos += pos
-
-# # Find min and max to create overlap array.
-# min_x, max_x = float('inf'), 0
-# for x1, x2 in movements:
-#     if x1 < min_x:
-#         min_x = x1
-#     if x2 > max_x:
-#         max_x = x2
-
-# # Iterate through the movements and find overlaps.
-# overlaps = [0 for _ in range(min_x, max_x + 1)]
-# for x1, x2 in movements:
-#     if min_x < 0:
-#         x1 += (min_x * -1)
-#         x2 += (min_x * -1)
-#     else:
-#         x1 += min_x
-#         x2 += min_x
-#     # Exclude the last iteration for overlap.
-#     for i in range(x1, x2): 
-#         overlaps[i] += 1
-
-# # Add the number of overlaps in the array
-# res = 0
-# for overlap in overlaps:
-#     if overlap >= 2:
-#         res += 1
-
-# print(res)
-
-
-
 n = int(input())
 movement = []
 for _ in range(n):
     xi, di = input().split()
     movement.append((int(xi), di))
 
-counter = {}
+# counter = {}
+# for i in range(-1000, 1000):
+#     counter[i] = 0
 
-for i in range(-1000, 1000):
-    counter[i] = 0
+# curr = 0
+# for pos, d in movement:
+#     if d == "L":
+#         pos *= -1
 
-curr = 0
-for pos, d in movement:
-    if d == "L":
-        pos *= -1
-
-    if curr + pos > curr:
-        for i in range(curr, curr + pos):
-            counter[i] += 1
-    else:
-        for i in range(curr + pos, curr):
-            counter[i] += 1
+#     if curr + pos > curr:
+#         for i in range(curr, curr + pos):
+#             counter[i] += 1
+#     else:
+#         for i in range(curr + pos, curr):
+#             counter[i] += 1
     
-    curr = curr + pos
+#     curr = curr + pos
 
-res = 0
-for count in counter.values():
-    if count >= 2:
-        res += 1 
+# res = 0
+# for count in counter.values():
+#     if count >= 2:
+#         res += 1 
 
-print(res)
+# print(res)
 
-# 1. prev_d != curr_d
+# Set up
+d = {}
+for i in range(-1000, 1000):
+    d[i] = False
+d[0] = True
 
-# 2. prev_d == curr_d
+# iter moves
+idx = 0
+s = set()
+for xi, di in movement:
+    if di == "R":
+        for j in range(idx, idx + xi):
+            if not d[j]:
+                d[j] = True
+            elif j not in s:
+                s.add(j)
+        idx = idx + xi
+
+    elif di == "L":
+        for j in range(idx - 1, idx - xi - 1, -1):
+            if not d[j]:
+                d[j] = True
+            elif j not in s:
+                s.add(j)
+        idx = idx - xi
+
+print(len(s))
