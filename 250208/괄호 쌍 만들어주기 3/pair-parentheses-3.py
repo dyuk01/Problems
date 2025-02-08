88bd1eee9659b85dd9ef016d05f5c8f92b2f087c
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-02-08 01:36:21 -0500

    Title: 괄호 쌍 만들어주기 3, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250208/괄호 쌍 만들어주기 3/pair-parentheses-3.py
+++ 250208/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -4,8 +4,8 @@ A = input()
 
 res = 0
 for i in range(len(A)):
-    for j in range(i + 1, len(A)):
-        if A[i] == '(':
+    if A[i] == '(':
+        for j in range(i + 1, len(A)):
             if A[j] == ')':
                 res += 1
 
