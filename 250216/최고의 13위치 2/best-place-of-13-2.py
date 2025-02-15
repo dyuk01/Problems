17c75f0575882b1b6880adb64430424c779e2024
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-02-15 17:33:42 -0500

    Title: 최고의 13위치 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250216/최고의 13위치 2/best-place-of-13-2.py
+++ 250216/최고의 13위치 2/best-place-of-13-2.py
@@ -1,38 +1,31 @@
 n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
 
-# coin : 1
-# No coin : 0
-# Need to overlap 1 * 3 blocks to find max coin
-# block is 3-wide, 1-tall
-
-# output will be 0~6, where you can just return immediately at 6
-
-def get_coin(i,j):
+# Write your code here!
+def get_coin(i, j):
     coin = 0
     for dj in range(3):
         new_j = j + dj
         if board[i][new_j] == 1:
             coin += 1
+    
     return coin
 
-# Find the first block with the most coin
+# Get first block's max coin
 max_coin1 = 0
 coin1_i, coin1_j = 0, 0
 for i in range(n):
     for j in range(n - 2):
-        if max_coin1 < get_coin(i,j):
-            max_coin1 = get_coin(i,j)
+        if max_coin1 < get_coin(i, j):
+            max_coin1 = get_coin(i, j)
             coin1_i, coin1_j = i, j
 
-# Then, find the second block with the most coin
+# Get second block's max coin
 max_coin2 = 0
 for i in range(n):
     for j in range(n - 2):
-        if coin1_i == i and coin1_j <= j <= coin1_j + 2 :
+        if i == coin1_i and coin1_j - 2 <= j <= coin1_j + 2:
             continue
-        
-        max_coin2 = max(max_coin2, get_coin(i,j))
-
+        max_coin2 = max(max_coin2, get_coin(i, j))
 
 print(max_coin1 + max_coin2)
