60ed38444ba271d55eb70316a4d7d02a7e69b2cc
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-02-15 18:04:09 -0500

    Title: 아름다운 수열 2, Time: 51ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250216/아름다운 수열 2/beautiful-sequence-2.py
+++ 250216/아름다운 수열 2/beautiful-sequence-2.py
@@ -5,13 +5,15 @@ sequence = list(map(int, input().split()))
 res = 0
 for i in range(len(numbers) - m + 1):
     seq_counter = 0
+    used = []
     for di in range(i, i + m):
         number = numbers[di]
-        if number not in sequence:
+        if number in sequence and number not in used:
+            used.append(number)
+            seq_counter += 1
+            if seq_counter == m:
+                res += 1
+        else:
             break
-
-        seq_counter += 1
-        if seq_counter == m:
-            res += 1
 
 print(res)
