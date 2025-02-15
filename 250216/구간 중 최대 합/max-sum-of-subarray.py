8f0f1dcbec9d575d6d490a479a6d7770c80b7da1
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-02-15 17:45:44 -0500

    Title: 구간 중 최대 합, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250216/구간 중 최대 합/max-sum-of-subarray.py
+++ 250216/구간 중 최대 합/max-sum-of-subarray.py
@@ -1,10 +1,18 @@
 n, k = list(map(int, input().split()))
 numbers = list(map(int, input().split()))
 
+def is_out_of_bound(i):
+    return i < 0 or i >= n
+
 max_sum = 0
-for i in range(len(numbers) - 2):
-    curr_sum = numbers[i] + numbers[i + 1] + numbers[i + 2] 
+for i in range(n):
+    curr_sum = 0
+    for di in range(k):
+        if is_out_of_bound(di):
+            break
+        curr_sum += numbers[di]
+    
     if max_sum < curr_sum:
         max_sum = curr_sum
-    
+
 print(max_sum)
