25fcdb7e07a636b19b260d30009cb046dfd7ddda
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-02-15 19:50:08 -0500

    Title: 바구니 안의 사탕 2, Time: 78ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250216/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250216/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -4,7 +4,7 @@ basket = [0 for _ in range(100)]
 
 for _ in range(n):
     c, p = map(int, input().split())
-    basket[p] = c
+    basket[p] += c
 
 # Return the max amount of candy after finding middle point c
 max_candy = 0
