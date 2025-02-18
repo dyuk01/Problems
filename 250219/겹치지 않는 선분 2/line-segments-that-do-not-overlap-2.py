3615ae6aa5f6fd26a53ee3633ddf0b54886c5233
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-02-18 12:53:23 -0500

    Title: 겹치지 않는 선분 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250219/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -0,0 +1,23 @@
+n = int(input())
+lines = [tuple(map(int, input().split())) for _ in range(n)]
+sorted_lines = sorted(lines, key=lambda x: x[0])
+
+# Logic : as long as the next line doesn't end before the previous line,
+# Lines do not overlap
+
+# Naive approach can be solved with Brute force O(n^2), but my approach would
+# sort and run through the input once. O(nlogn)
+
+# Return the number of non-overlapping lines
+
+
+appropriate_lines = n
+for i in range(n - 1):
+    curr_begin_x, curr_end_x = sorted_lines[i]
+    next_begin_x, next_end_x = sorted_lines[i + 1]
+
+    if next_end_x < curr_end_x:
+        appropriate_lines -= 2
+        i += 2
+
+print(appropriate_lines)
