50f54416d794d95614b0d81730c8b0d55b153d07
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-03-01 01:09:41 -0500

    Title: 야바위, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250301/야바위/ya-rock.py
+++ 250301/야바위/ya-rock.py
@@ -1,17 +1,34 @@
 n = int(input())
 moves = [tuple(map(int, input().split())) for _ in range(n)]
 
-# Please write your code here.
-max_score = -1
-for index in range(1, 4):
-    cups = [0] * 4
-    cups[0] = -1
-    cups[index] = 1
-    score = 0
-    for a,b,c in moves:
-        cups[a], cups[b] = cups[b], cups[a]
-        score += cups[c]
+# # Please write your code here.
+# max_score = -1
+# for index in range(1, 4):
+#     cups = [0] * 4
+#     cups[0] = -1
+#     cups[index] = 1
+#     score = 0
+#     for a,b,c in moves:
+#         cups[a], cups[b] = cups[b], cups[a]
+#         score += cups[c]
     
-    max_score = max(max_score, score)
+#     max_score = max(max_score, score)
 
-print(max_score)
+# print(max_score)
+
+cup_map = {
+    1: 1,
+    2: 2,
+    3: 3,
+}
+
+point_map = {
+    1: 0,
+    2: 0,
+    3: 0,
+}
+
+for a, b, c in moves:
+    cup_map[a], cup_map[b] = cup_map[b], cup_map[a]
+    point_map[cup_map[c]] += 1
+print(max(point_map.values()))
