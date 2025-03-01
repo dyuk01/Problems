6bda3d0043afc6b2a7db3cf11e2e5e9fbaedb73e
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-03-01 01:55:37 -0500

    Title: 3개의 선 2, Time: 82ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250301/3개의 선 2/three-lines-2.py
@@ -0,0 +1,43 @@
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+
+def sol():
+    x_coords = sorted(list(set(p[0] for p in points)))
+    y_coords = sorted(list(set(p[1] for p in points)))
+
+    if len(x_coords) <= 3 or len(y_coords) <= 3:
+        return 1
+    
+    for i in range(len(y_coords)):
+        for j in range(i + 1, len(x_coords)):
+            crossed = set()
+            for p in points:
+                if p[1] == y_coords[i] or p[1] == y_coords[j]:
+                    crossed.add(p)
+        
+
+            remain = set(points) - crossed
+            if not remain:
+                return 1
+            
+            remain_x = set(p[0] for p in remain)
+            if len(remain_x) == 1:
+                return 1
+    
+    for i in range(len(x_coords)):
+        for j in range(i + 1, len(x_coords)):
+            crossed = set()
+            for p in points:
+                if p[0] == x_coords[i] or p[0] == x_coords[j]:
+                    crossed.add(p)
+            
+            remain = set(points) - crossed
+            if not remain:
+                return 1
+            remain_y = set([1] for p in remain)
+            if len(remain_y) == 1:
+                return 1
+    
+    return 0
+
+print(sol())
