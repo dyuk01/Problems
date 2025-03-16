22e6f9c6334e4b5dab78a21bf8505159308393d8
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-03-16 01:03:13 -0400

    Title: 수들의 최대 차, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250316/수들의 최대 차/maximum-difference-in-numbers.py
+++ 250316/수들의 최대 차/maximum-difference-in-numbers.py
@@ -1,36 +1,49 @@
+# n, k = map(int, input().split())
+# arr = [int(input()) for _ in range(n)]
+
+# arr.sort()
+
+# arr_min = []
+# arr_max = []
+# for a in arr:
+#     arr_min.append(a)
+#     arr_max.append(a)
+
+# # Please write your code here.
+# def remove_min(s):
+#     diff = max(s) - min(s)
+
+#     while diff > k:
+#         s.pop(0)
+#         diff = max(s) - min(s)
+
+#     return len(s)
+
+
+# def remove_max(s):
+#     diff = max(s) - min(s)
+
+#     while diff > k:
+#         s.pop(-1)
+#         diff = max(s) - min(s)
+
+#     return len(s)
+
+# res_min = remove_min(arr_min)
+# res_max = remove_max(arr_max)
+
+# print(max(res_min, res_max))
+
 n, k = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
+arr.sort()
 
-arr_min = []
-for a in arr:
-    arr_min.append(a)
+def solution() -> int:
+    best_len = -float("inf")
+    for l in range(n - 1):
+        for r in range(l + 1, n):
+            if arr[r] - arr[l] < k:
+                best_len = max(best_len, r - l + 1)
+    return best_len
 
-arr_max = []
-for a in arr:
-    arr_max.append(a)
-
-# Please write your code here.
-def remove_min(s):
-    diff = max(s) - min(s)
-
-    while diff > k:
-        s.pop(s.index(min(s)))
-        diff = max(s) - min(s)
-
-    return len(s)
-
-
-def remove_max(s):
-    diff = max(s) - min(s)
-
-    while diff > k:
-        s.pop(s.index(max(s)))
-        diff = max(s) - min(s)
-
-    return len(s)
-
-res_min = remove_min(arr_min)
-res_max = remove_max(arr_max)
-
-print(max(res_min, res_max))
-   
+print(solution() + 1)
