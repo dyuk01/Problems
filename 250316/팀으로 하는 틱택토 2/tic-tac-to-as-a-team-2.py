f854383a4f74c7b7442e67bb49473afd50a5a2c6
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-03-15 22:51:06 -0400

    Title: 팀으로 하는 틱택토 2, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250316/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 250316/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -3,33 +3,38 @@ inp = [input() for _ in range(3)]
 # Please write your code here.
 
 res = 0
+answers = []
 
 # Count answer vertically
 for i in range(3):
-    if len(set(inp[i])) == 2:
+    if len(set(inp[i])) == 2 and set(inp[i]) not in answers:
         res += 1
+        answers.append(set(inp[i]))
 
 # Count answer horizontally
 for j in range(3):
     num_v = set()
     for i in range(3):
         num_v.add(inp[i][j])
-    if len(set(num_v)) == 2:
+    if len(set(num_v)) == 2 and num_v not in answers:
         res += 1
+        answers.append(num_v)
 
 # Count answer diagonally
 num_d = set()
 for i in range(3):
     num_d.add(inp[i][i])
 
-if len(set(num_d)) == 2:
+if len(set(num_d)) == 2 and num_d not in answers:
     res += 1
+    answers.append(num_d)
 
 num_d = set()
 for i in range(3):
     num_d.add(inp[i][2 - i])
 
-if len(set(num_d)) == 2:
+if len(set(num_d)) == 2 and num_d not in answers:
     res += 1
+    answers.append(num_d)
 
 print(res)
