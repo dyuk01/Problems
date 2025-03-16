eeb235b4eb94c6f9c635e9013398e44aeff08374
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-03-15 23:14:00 -0400

    Title: 팀으로 하는 틱택토 2, Time: 94ms, Memory: 19MB, Status: Passed - CodeTree

--- 250316/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 250316/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,40 +1,69 @@
+# inp = [input() for _ in range(3)]
+
+# # Please write your code here.
+
+# def check_team_win(s):
+#     if len(s) == 2 and s not in answers:
+#         answers.append(s)
+
+# answers = []
+
+# # Count answer horizontally
+# for i in range(3):
+#     check_team_win(set(inp[i]))
+
+# # Count answer vertically
+# for j in range(3):
+#     num_v = set()
+#     for i in range(3):
+#         num_v.add(inp[i][j])
+
+#     check_team_win(num_v)
+
+# # Count answer diagonally
+# num_d = set()
+# for i in range(3):
+#     num_d.add(inp[i][i])
+
+# check_team_win(num_d)
+
+# num_d = set()
+# for i in range(3):
+#     num_d.add(inp[i][2 - i])
+
+# check_team_win(num_d)
+
+# print(len(answers))
+
+
+
+from typing import Set
+
 inp = [input() for _ in range(3)]
 
-# Please write your code here.
-
-res = 0
 answers = []
 
-# Count answer vertically
-for i in range(3):
-    if len(set(inp[i])) == 2 and set(inp[i]) not in answers:
-        res += 1
-        answers.append(set(inp[i]))
+# Check the directional set to see if there is a team win.
+def check_team_win(s: Set[int]) -> None:
+    if len(s) == 2 and s not in answers:
+        answers.append(s)
 
-# Count answer horizontally
-for j in range(3):
-    num_v = set()
+def solution() -> int:
+    # Horizontal.
     for i in range(3):
-        num_v.add(inp[i][j])
-    if len(set(num_v)) == 2 and num_v not in answers:
-        res += 1
-        answers.append(num_v)
+        check_team_win(set(inp[i]))
+    
+    # Vertical.
+    for j in range(3):
+        check_team_win({inp[0][j], inp[1][j], inp[2][j]})
+    
+    # Bottom-right diagonal.
+    check_team_win({inp[0][0], inp[1][1], inp[2][2]})
 
-# Count answer diagonally
-num_d = set()
-for i in range(3):
-    num_d.add(inp[i][i])
+    # Bottom-left diagonal.
+    check_team_win({inp[0][2], inp[1][1], inp[2][0]})
 
-if len(set(num_d)) == 2 and num_d not in answers:
-    res += 1
-    answers.append(num_d)
+    return len(answers)
 
-num_d = set()
-for i in range(3):
-    num_d.add(inp[i][2 - i])
-
-if len(set(num_d)) == 2 and num_d not in answers:
-    res += 1
-    answers.append(num_d)
-
-print(res)
+print(solution())
+    
