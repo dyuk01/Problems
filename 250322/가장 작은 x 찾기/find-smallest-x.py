a39f2770369d5f8b9d8bfffbd71112ea11bdeaaa
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-03-22 09:13:44 -0400

    Title: 가장 작은 x 찾기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250322/가장 작은 x 찾기/find-smallest-x.py
+++ 250322/가장 작은 x 찾기/find-smallest-x.py
@@ -8,24 +8,12 @@ a, b = list(a), list(b)
 # Please write your code here.
 # a_i <= n * 2^(i+1) <= b_i
 
-# res = []
-# for i in range(n):
-#     ceil = math.ceil(a[i] / pow(2, i+1))
-#     floor = math.floor(b[i] / pow(2, i+1))
-#     print(ceil, floor)
-#     for j in range(ceil, floor + 1):
-#         if j not in res:
-#             res.append(j)
-#         else:
-#             res.remove(j)
+intervals = []
+for i in range(n):
+    factor = pow(2, i+1)
+    ceil = math.ceil(a[i] / factor)
+    floor = math.floor(b[i] / factor)
+    intervals.append((ceil, floor))
 
-# print(res)
-
-res = []
-ceil = math.ceil(a[n - 1] / pow(2, n))
-floor = math.floor(b[n - 1] / pow(2, n))
-
-for i in range(ceil, floor + 1):
-    res.append(i)
-
-print(min(res))
+res = max(r[0] for r in intervals)
+print(res)
