3ba08cea118e24b3a3c6d16030c034b7d19b375c
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-03-23 00:00:47 -0400

    Title: 독서실의 거리두기 4, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250323/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -0,0 +1,28 @@
+n = int(input())
+seats = input()
+
+# Please write your code here.
+occupied = []
+
+for i in range(n):
+    if seats[i] == '1':
+        occupied.append(i)
+
+max_min_dist = 0
+
+for i in range(n - 1):
+    if seats[i] == '0':
+        temp_occupied = occupied + [i]
+    for j in range(i + 1, n):
+        if seats[j] == '0':
+            temp_occupied = occupied + [j]
+            temp_occupied.sort()
+
+            min_dist = float('inf')
+            for k in range(1, len(temp_occupied)):
+                dist = temp_occupied[k] - temp_occupied[k - 1]
+                min_dist = min(min_dist, dist)
+            
+            max_min_dist = max(max_min_dist, min_dist)
+
+print(max_min_dist)
