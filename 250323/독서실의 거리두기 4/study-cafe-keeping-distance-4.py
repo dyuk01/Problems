7b4c8cb9bd2001230be184dab5bfb7bd336a69f2
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-03-23 01:00:29 -0400

    Title: 독서실의 거리두기 4, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250323/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
+++ 250323/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -30,6 +30,24 @@ seats = input()
 NUM_PEOPLE = 2
 
 pos = [i for i, o in enumerate(seats) if o == "1"]
+default_min_dist = n
+for i in range(1, len(pos)):
+    default_min_dist = min(default_min_dist, pos[i] - pos[i-1])
+
+# def get_min_both(i: int) -> int:
+#     # Left min dist.
+#     left_dist = float("inf")
+#     right_dist = float("inf")
+#     for j in range(i - 1, -1, -1):
+#         if seats[j] == "1":
+#             left_dist = i - j
+#             break
+#     for j in range(i + 1, n):
+#         if seats[j] == "1":
+#             left_dist = j - i
+#             break
+
+#     return min(left_dist, right_dist)
 
 def check_dist(d: int) -> bool:
     i = 0
@@ -39,7 +57,7 @@ def check_dist(d: int) -> bool:
         if seats[i] == "0":
             # Check with existing 1s.
             valid = True
-            if min([abs(p - i) for p in pos]) < d:
+            if min([abs(p - i) for p in pos] + [default_min_dist]) < d:
                 valid = False
                 
             # Check with newly added 1s.
@@ -53,16 +71,15 @@ def check_dist(d: int) -> bool:
 
 # Utilize binary search to maximize the possible minimum distance between 1s.
 def solution() -> int:
-    lo, hi = 0, n
+    lo, hi = 1, n
     result = 0
     while lo <= hi:
         mid = (lo + hi) // 2
-        if check_dist(mid):
+        if mid > default_min_dist or not check_dist(mid):
+            hi = mid - 1
+        else:
             result = mid
             lo = mid + 1
-        else:
-            hi = mid - 1
-    
     return result
 
 print(solution())
