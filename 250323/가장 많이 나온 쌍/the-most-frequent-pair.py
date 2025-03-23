0b32d4f4d81b2d4444621dd486c14b427eb8c2d6
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-03-22 23:48:48 -0400

    Title: 가장 많이 나온 쌍, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250323/가장 많이 나온 쌍/the-most-frequent-pair.py
+++ 250323/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -2,15 +2,26 @@ n, m = map(int, input().split())
 pairs = [tuple(map(int, input().split())) for _ in range(m)]
 
 # Please write your code here.
-normalized = list(map(lambda x: (min(x), max(x)), pairs))
-normalized.sort()
+# normalized = list(map(lambda x: (min(x), max(x)), pairs))
+# normalized.sort()
 
-max_count = count = 1
-for i in range(1, m):
-    if normalized[i] == normalized[i - 1]:
-        count += 1
-        max_count = max(max_count, count)
+# max_count = count = 1
+# for i in range(1, m):
+#     if normalized[i] == normalized[i - 1]:
+#         count += 1
+#         max_count = max(max_count, count)
+#     else:
+#         count = 1
+
+# print(max_count)
+
+cnt_dict = {}
+
+for pair in pairs:
+    pair = (min(pair), max(pair))
+    if pair not in cnt_dict:
+        cnt_dict[pair] = 1
     else:
-        count = 1
+        cnt_dict[pair] += 1
 
-print(max_count)
+print(max(cnt_dict.values()))
