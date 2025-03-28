467a5a5864df60e6fe5dce0d810fcbffc27210ec
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-03-28 15:22:35 -0400

    Title: 초기 수열 복원하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250329/초기 수열 복원하기/restore-initial-sequence.py
@@ -0,0 +1,27 @@
+n = int(input())
+adjacent = list(map(int, input().split()))
+
+# Please write your code here.
+
+def restore_seq(arr)-> list:
+    res = []
+    idx = 0
+
+    for i in range(1, adjacent[0]):
+        init_num = i
+        res.append(init_num)
+        for j in arr:
+            num = j - init_num
+            if num not in res:
+                res.append(num)
+            else:
+                res.clear()
+                break
+            init_num = num
+
+        if len(res) == n:
+            return res
+
+    return res
+
+print(' '.join(map(str, restore_seq(adjacent))))
