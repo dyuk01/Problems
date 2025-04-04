2996a37be29d0a5b6865a4da909e5cb068ec8514
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-04-04 06:50:38 -0400

    Title: 두 선분, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250404/두 선분/two-lines.py
+++ 250404/두 선분/two-lines.py
@@ -3,10 +3,17 @@ x1, x2, x3, x4 = map(int, input().split())
 # Please write your code here.
 
 
-# Case1: 3 < 2
-# Case2: 1 < 4
+def solution()-> str:
+    # Case1: 3 < 2
+    if x1 <= x3:
+        if x3 <= x2:
+            return "intersecting"
 
-if x3 < x2 or x1 < x4:
-    print("intersecting")
-else:
-    print("nonintersecting")
+    # Case2: 1 < 4
+    elif x3 <= x1:
+        if x1 <= x4:
+            return "intersecting"
+    
+    return "nonintersecting"
+
+print(solution())
