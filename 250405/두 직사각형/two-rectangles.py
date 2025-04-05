7083717f4d601f9864b62b96fb9aa0a65187af41
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-04-04 21:51:21 -0400

    Title: 두 직사각형, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250405/두 직사각형/two-rectangles.py
+++ 250405/두 직사각형/two-rectangles.py
@@ -3,4 +3,27 @@ a1, b1, a2, b2 = map(int, input().split())
 
 # Please write your code here.
 
-# Case1: x2 < a1
+def sol():
+    # Case1: x2 <= a1
+    if x1 <= a1:
+        if x2 <= a1:
+            return "intersecting"
+
+    # Case2: a2 <= x1
+    elif a1 <= x1:
+        if a2 <= x1:
+            return "intersecting"
+
+    # Case3: y2 <= b1
+    if y1 <= a1:
+        if y2 <= b1:
+            return "intersecting"
+
+    # Case4: b2 <= y1
+    elif a1 <= y1:
+        if b2 <= y1:
+            return "intersecting"
+    
+    return "nonintersecting"
+
+print(sol())
