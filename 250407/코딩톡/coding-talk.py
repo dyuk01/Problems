dc1987a39c06398a80fac3f765718017c0597f3b
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-04-07 00:41:56 -0400

    Title: 코딩톡, Time: 70ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250407/코딩톡/coding-talk.py
+++ 250407/코딩톡/coding-talk.py
@@ -16,26 +16,18 @@ p -= 1
 # Find who did not read the pth message
 people = [chr(i) for i in range(ord('A'), ord('A') + n)]
 
-def sol(arr):
+def add_suspect(arr):
     if u[p] == 0:
         return ""
 
     # Go directly to pth message (1-indexed)
     participants = set()
 
-    # Iterate from the pth to the end, and add to set()
-    for i in range(p, m):
-        if c[i] not in participants:
-            participants.add(c[i])
-
-    # Subtract the elements from c to set(), to find who
-    # did not participate in the conversation + self
-    for person in people: 
-        if person not in participants:
-            arr.append(person)
-    arr.sort()
-    return arr
+    
 
 
 suspects = []
-print(" ".join(sol(suspects)))
+for i in range(m):
+    suspects = add_suspect(suspects)
+
+print(" ".join(suspects))
