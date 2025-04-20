0f1dcef3707ecf1f269da4e490d4313d9a1c987c
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-04-20 02:01:37 -0400

    Title: 독서실의 거리두기 2, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250420/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 250420/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -2,33 +2,49 @@ n = int(input())
 seats = input()
 
 # Please write your code here.
-occupied = []
-for idx, s in enumerate(seats): 
-    if s == '1':
-        occupied.append(idx)
+# occupied = []
+# for idx, s in enumerate(seats): 
+#     if s == '1':
+#         occupied.append(idx)
 
-# print(occupied)
+# max_min_dist = -1
 
-open_distances = []
-closed_distances = []
-# 0 4 7 11
+# for i in range(len(occupied) - 2):
+#     l = occupied[i]
+#     r = occupied[i + 1]
 
-# Case1: Beginnig Empty
-if occupied[0] != 0:
-    open_distances.append(occupied[0])
+#     if r - l > 1:
+#         mid = (l + r) // 2
+    
+#     min_dist = mid - l
+#     max_min_dist = max(max_min_dist, min_dist)
 
-i, j = 0, 1
-while j < len(occupied):
-    closed_distances.append(occupied[j] - occupied[i])
-    i += 1
-    j += 1
 
-# Case2: End Empty
-if occupied[-1] != n - 1:
-    open_distances.append(n - 1 - occupied[-1])
+# if occupied[0] > 0:
+#     min_dist = occupied[0]
+#     max_min_dist = max(max_min_dist, min_dist)
 
-new_closed_distances = []
-for dist in closed_distances:
-    new_closed_distances.append(dist // 2)
+# if occupied[-1] < n - 1:
+#     min_dist = n - 1 - occupied[-1]
+#     max_min_dist = max(max_min_dist, min_dist)
 
-print(max(max(open_distances), max(new_closed_distances)))
+# print(max_min_dist)
+
+
+
+def solution() -> int:
+    occupied = [i for i, s in enumerate(seats) if s == "1"]
+    
+    max_dist = -float('inf')
+    for i in range(n):
+        if seats[i] == 1:
+            continue
+        
+        test = occupied + [i]
+        test.sort()
+        min_dist = min(test[j + 1] - test[j] for j in range(len(test) - 1))
+        max_dist = max(max_dist, min_dist)
+
+    return max_dist
+
+print(solution())
