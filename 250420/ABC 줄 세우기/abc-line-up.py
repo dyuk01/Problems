79caee02985c2224dd1962f6606f46771c3de549
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-04-20 02:19:54 -0400

    Title: ABC 줄 세우기, Time: 40ms, Memory: 16MB, Status: Passed - Codetree

--- 250420/ABC 줄 세우기/abc-line-up.py
+++ 250420/ABC 줄 세우기/abc-line-up.py
@@ -2,14 +2,24 @@ n = int(input())
 arr = list(input().split())
 
 # Please write your code here.
-sorted_arr = []
-for ch in arr:
-    sorted_arr.append(ord(ch))
+# sorted_arr = []
+# for ch in arr:
+#     sorted_arr.append(ord(ch))
 
-sorted_arr.sort()
+# sorted_arr.sort()
 
-max_moves = -1
+# max_moves = -1
+# for i in range(n):
+#     max_moves = max(max_moves, abs(ord(arr[i]) - sorted_arr[i]))
+
+# print(max_moves)
+
+swap_count = 0
+
 for i in range(n):
-    max_moves = max(max_moves, abs(ord(arr[i]) - sorted_arr[i]))
+    for j in range(0, n - i - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+            swap_count += 1
 
-print(max_moves)
+print(swap_count)
