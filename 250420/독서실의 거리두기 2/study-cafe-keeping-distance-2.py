e71a3f0a2619aa61de8866a299864afce01464ad
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-04-20 01:44:02 -0400

    Title: 독서실의 거리두기 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250420/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 250420/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -7,5 +7,28 @@ for idx, s in enumerate(seats):
     if s == '1':
         occupied.append(idx)
 
+# print(occupied)
 
+open_distances = []
+closed_distances = []
 # 0 4 7 11
+
+# Case1: Beginnig Empty
+if occupied[0] != 0:
+    open_distances.append(occupied[0])
+
+i, j = 0, 1
+while j < len(occupied):
+    closed_distances.append(occupied[j] - occupied[i])
+    i += 1
+    j += 1
+
+# Case2: End Empty
+if occupied[-1] != n - 1:
+    open_distances.append(n - 1 - occupied[-1])
+
+new_closed_distances = []
+for dist in closed_distances:
+    new_closed_distances.append(dist // 2)
+
+print(max(max(open_distances), max(new_closed_distances)))
