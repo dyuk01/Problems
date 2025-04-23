98838683b97b214831b7cd654f53d989f0708fa5
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-04-23 13:18:25 -0400

    Title: 최소 와이파이 수, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250424/최소 와이파이 수/minimum-number-of-wifi.py
+++ 250424/최소 와이파이 수/minimum-number-of-wifi.py
@@ -2,31 +2,11 @@ n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
-arr.append(0)
 
 def solution()-> int:
-    # Base case
-    if m == 0:
-        return sum(arr)
-    
-    # Keep track of how many people there are
-    area_populated = []
-    count = 0
-    for a in arr:
-        if a == 1:
-            count += 1
-        
-        if a == 0 and count != 0:
-            area_populated.append(count)
-            count = 0
-    
-    wifi = 0
+    total_population = sum(arr)
     wifi_power = 2 * m + 1
-    for people in area_populated:
-        if people % wifi_power == 0:
-            wifi += people // wifi_power
-        else:
-            wifi += people // wifi_power + 1
+    wifi = total_population // wifi_power if total_population % wifi_power == 0 else total_population // wifi_power + 1
     
     return wifi
 
