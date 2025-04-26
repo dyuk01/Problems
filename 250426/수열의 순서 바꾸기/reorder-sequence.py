af6b58099bdc1ee2e03a6f5f8ea414d374a63192
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-04-26 10:52:51 -0400

    Title: 수열의 순서 바꾸기, Time: 1020ms, Memory: 19MB, Status: Time Limit Exceed - Codetree

--- 250426/수열의 순서 바꾸기/reorder-sequence.py
+++ 250426/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,21 +1,47 @@
+from typing import List
+
 n = int(input())
-seq = list(map(int, input().split()))
+sequence = list(map(int, input().split()))
 
-# Please write your code here.
+def inc_tail_start(seq: List[int]) -> int:
+    i = len(seq) - 1
+    while i >= 0:
+        if seq[i] != i + 1:
+            return i
+    return -1
 
-# Type of insertion sort -> sort in non-decreasing order
+def is_sorted(seq: List[int]) -> bool:
+    for i, x in enumerate(seq):
+        if x != i + 1:
+            return False
+    return True
 
-# Pick a number at beginning
+def find_moves(seq: List[int], moves: int) -> int:
+    if is_sorted(seq):
+        return moves
+    
+    t = inc_tail_start(seq)
+    n = t + 1
+    seq = seq[:t+1]
+    while not is_sorted(seq):
+        x = seq.pop(0)
 
-sorted_tail = 1
+        if x == n:
+            seq.append(x)
+            moves += 1
+            break
+        
+        n_idx = seq.index(n)
+        insert_idx = n_idx + 1
+        while x > seq[insert_idx] and insert_idx < n:
+            insert_idx += 1
+        if insert_idx == n:
+            seq.append(x)
+        else:
+            seq.insert(insert_idx, x)
+        
+        moves += 1
 
-for i in range(n - 2, -1, -1):
-    if seq[i] < seq[i + 1]:
-        sorted_tail += 1
-    else:
-        break
+    return find_moves(seq, moves)
 
-print(n - sorted_tail)
-
-
-
+print(find_moves(sequence, 0))
