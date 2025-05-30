3c9df04a46e08c1897ae697a15227dc2a35abd6b
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-05-30 22:53:21 +0900

    Title: 황금비율 토스트, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250530/황금비율 토스트/golden-toast.py
+++ 250530/황금비율 토스트/golden-toast.py
@@ -1,55 +1,117 @@
-n, m = map(int, input().split())
-s = input()
+# n, m = map(int, input().split())
+# s = input()
 
-commands = []
-for _ in range(m):
-    cmd = input().split()
-    if len(cmd) == 1:
-        commands.append((cmd[0],))
-    else:
-        commands.append((cmd[0], cmd[1]))
+# commands = []
+# for _ in range(m):
+#     cmd = input().split()
+#     if len(cmd) == 1:
+#         commands.append((cmd[0],))
+#     else:
+#         commands.append((cmd[0], cmd[1]))
 
-# Please write your code here.
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.prev = None
-        self.next = None
+# # Please write your code here.
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.prev = None
+#         self.next = None
 
-class DoubleLinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        self.curr_node = None
-
-
-    def left(self, curr_node):
-        if curr_node.prev == None:
-            continue
-        else:
-            curr_node = curr_node.prev
+# class DoubleLinkedList:
+#     def __init__(self):
+#         self.head = None
+#         self.tail = None
+#         self.curr = None
 
 
-    def right(self, curr_node):
-        if curr_node.next == None:
-            continue
-        else:
-            curr_node = curr_node.next
+#     def left(self):
+#         if self.curr == self.head:
+#             return
+#         self.curr = self.curr.prev
+
+
+#     def right(self):
+#         if self.curr == self.tail:
+#             return
+#         self.curr = self.curr.next
     
 
-    def delete(self, curr_node):
-        # Empty -> pass
-        if curr_node.next == None:
-            continue
-        else:
-            # Last node
-            if curr_node.next.next == None:
-                curr_node.next.prev = None
-                curr_node.prev = None
-            else:
+#     def delete(self):
+#         # Empty -> pass
+#         if self.curr == self.tail:
+#             return
 
-            
+#         # Delete last node
+#         if self.curr.next == self.tail:
+#             self.tail.prev = None
+#             self.curr.next = None
+#             self.curr = self.tail
 
-    def paste(self): 
-        pass
+#         # Delete middle node
+#         else:
+#             self.curr.next.next.prev = self.curr
+#             self.curr.next = self.curr.next.next
+#             self.curr.next.next = None
+#             self.curr.next.prev = None
+
+
+#     def push_back(self, c):
+#         new_node = Node(c)
+#         if self.head is None:
+#             self.head = new_node
+#             self.tail = new_node
+#             self.curr = new_node
+#             return
         
+#         self.tail.next = new_node
+#         new_node.prev = self.tail
+#         self.tail = new_node
+#         self.curr = self.tail
+
+
+#     def insert(self, c):
+#         new_node = Node(c)
+
+#         if self.curr != self.tail:
+#             # Add as middle
+#             self.curr.next.prev = new_node
+#             new_node.next = self.curr.next
+#             new_node.prev = self.curr
+#             self.curr.next = new_node
+#         else:
+#             # Add as tail
+#             new_node.prev = self.tail
+#             self.tail.next = new_node
+#             self.tail = new_node
+        
+#         if self.curr != None:
+#             self.curr = self.curr.next
+    
+
+#     def print_list(self):
+#         node = self.head
+#         while node:
+#             print(node.data, end='')
+#             node = node.next
+
+
+# dll = DoubleLinkedList()
+
+# # Initialize linked list
+# for c in s:
+#     dll.push_back(c)
+
+# # Follow the directions
+# for i in range(m):
+#     direction = commands[i][0]
+#     if direction == 'L':
+#         dll.left()
+#     elif direction == 'R':
+#         dll.right()
+#     elif direction == 'P':
+#         dll.insert(commands[i][1])
+#     else:
+#         dll.delete()
+
+# dll.print_list()
+    
+    
