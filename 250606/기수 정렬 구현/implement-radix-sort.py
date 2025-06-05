73eea0d81b740eabf139e35795ece345b807b528
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-06-06 00:56:03 +0900

    Title: 기수 정렬 구현, Time: 91ms, Memory: 20MB, Status: Runtime Error - Codetree

--- 250606/기수 정렬 구현/implement-radix-sort.py
+++ 250606/기수 정렬 구현/implement-radix-sort.py
@@ -1,39 +1,72 @@
+# n = int(input())
+# arr = list(map(int, input().split()))
+
+# # Please write your code here.
+
+# max_num = max(arr)
+# k = len(str(max_num))
+
+# str_arr = []
+# for num in arr:
+#     str_num = str(num)
+
+#     padded_num = ""
+#     if len(str_num) != k:
+#         for _ in range(k - len(str_num)):
+#             padded_num += "0"
+#     str_arr.append(padded_num + str_num)
+
+# for pos in range(k - 1, -1, -1):
+#     arr_new = [[] for _ in range(10)]
+
+#     for str_num in str_arr:
+#         digit = int(str_num[pos])        
+#         arr_new[digit].append(str_num)
+    
+#     str_arr = []
+#     for num_new in arr_new:
+#         str_arr.extend(num_new)
+
+# cleansed_res = []
+
+# for s in str_arr:
+#     cleansed_res.append(s)
+
+# print(' '.join(map(str, map(int, cleansed_res))))
+
+from typing import List, Dict
+
 n = int(input())
 arr = list(map(int, input().split()))
 
-# Please write your code here.
-
-max_num = max(arr)
-k = len(str(max_num))
-
-str_arr = []
-for num in arr:
-    str_num = str(num)
-
-    padded_num = ""
-    if len(str_num) != k:
-        for _ in range(k - len(str_num)):
-            padded_num += "0"
-    str_arr.append(padded_num + str_num)
-
-for pos in range(k - 1, -1, -1):
-    arr_new = [[] for _ in range(10)]
-
-    for str_num in str_arr:
-        digit = int(str_num[pos])        
-        arr_new[digit].append(str_num)
+def pad_nums(n: int, digit: int) -> str:
+    # return str(n).zfill(digit)
+    # return f"{n:0{digit}d}"
+    x = ""
+    for _ in range(digit - len(str(n))):
+        n += "0"
+    return x + str(n)
     
-    str_arr = []
-    for num_new in arr_new:
-        str_arr.extend(num_new)
 
-cleansed_res = []
-# for i in range(len(str_arr)):
-#         sorted_num = str_arr[i]
+def flatten(radix: Dict[int, List[str]]) -> List[str]:
+    temp = []
+    for i in range(10):
+        temp.extend(radix[i])
+    return temp
 
-# print(' '.join(map(str, cleansed_res)))
+def radix_sort() -> List[str]:
+    k = len(str(max(arr)))
+    pad_arr = [pad_nums(num, k) for num in arr]
 
-for s in str_arr:
-    cleansed_res.append(s)
+    for pos in range(k - 1, -1, -1):
+        radix = {i: [] for i in range(10)}
+        for num in pad_arr:
+            radix[int(num[pos])].append(num)
+        pad_arr = flatten(radix)
 
-print(' '.join(map(str, map(int, cleansed_res))))
+    return pad_arr
+
+def solution() -> str:
+    return " ".join(map(str, list(map(int, radix_sort()))))
+
+print(solution())
