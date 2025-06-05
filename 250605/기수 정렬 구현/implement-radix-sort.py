1f62467470b9cc2f82ee5e7415b01d269d6de352
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-06-05 23:36:47 +0900

    Title: 기수 정렬 구현, Time: 86ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250605/기수 정렬 구현/implement-radix-sort.py
@@ -0,0 +1,27 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+max_num = max(arr)
+k = len(str(max_num))
+
+for pos in range(k - 1, -1, -1):
+    arr_new = [[] for _ in range(10)]
+
+    for num in arr:
+        digit = num // (10 ** pos)
+        arr_new[digit].append(num)
+    
+    res = []
+    for num_new in arr_new:
+        res.append(num_new)
+
+cleaned_res = []
+for i in range(len(res)):
+    for j in range(len(res[i])):
+        sorted_num = res[i][j]
+        if sorted_num is not None:
+            cleaned_res.append(sorted_num)
+
+print(' '.join(map(str, cleaned_res)))
