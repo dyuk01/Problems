5bc430a36694a82178bea9001b1d65f04882f131
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-06-05 23:50:32 +0900

    Title: 기수 정렬 구현, Time: 73ms, Memory: 20MB, Status: Wrong Answer - Codetree

--- 250605/기수 정렬 구현/implement-radix-sort.py
+++ 250605/기수 정렬 구현/implement-radix-sort.py
@@ -6,16 +6,18 @@ arr = list(map(int, input().split()))
 max_num = max(arr)
 k = len(str(max_num))
 
+str_arr = [f"{num:0{k}d}" for num in arr]
+
 for pos in range(k - 1, -1, -1):
     arr_new = [[] for _ in range(10)]
 
-    for num in arr:
-        digit = (num // (10 ** pos)) % 10
-        arr_new[digit].append(num)
+    for str_num in str_arr:
+        digit = int(str_num[pos])        
+        arr_new[digit].append(str_num)
     
-        res = []
-        for num_new in arr_new:
-            res.append(num_new)
+    res = []
+    for num_new in arr_new:
+        res.append(num_new)
 
 cleaned_res = []
 for i in range(len(res)):
