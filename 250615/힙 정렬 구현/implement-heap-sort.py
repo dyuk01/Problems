a710930539a6bb92bb369549eff7d4a7bc743e62
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-06-15 13:56:04 +0900

    Title: 힙 정렬 구현, Time: 545ms, Memory: 26MB, Status: Passed - Codetree

--- 250615/힙 정렬 구현/implement-heap-sort.py
+++ 250615/힙 정렬 구현/implement-heap-sort.py
@@ -1,36 +1,68 @@
-from typing import List
+# from typing import List
+
+# n = int(input())
+# arr = list(map(int, input().split()))
+
+# # Please write your code here.
+
+# # Compare and sort
+# def heapify(arr: List[int], n: int, i: int) -> None:
+#     largest = i
+#     left = 2 * i
+#     right = 2 * i + 1
+
+#     if left < n and arr[left] > arr[largest]:
+#         largest = left
+    
+#     if right < n and arr[right] > arr[largest]:
+#         largest = right
+    
+#     if largest != i:
+#         arr[i], arr[largest] = arr[largest], arr[i]
+#         heapify(arr, n, largest)
+
+
+# def heap_sort(arr: List[int]) -> List[int]:
+#     for i in range(n//2 - 1, -1, -1):
+#         heapify(arr, n, i)
+    
+#     for i in range(n - 1, 0, -1):
+#         arr[0], arr[i] = arr[i], arr[0]
+#         heapify(arr, i, 0)
+    
+#     return arr
+
+# sorted_arr = heap_sort(arr)
+# print(' '.join(map(str, sorted_arr)))
 
 n = int(input())
 arr = list(map(int, input().split()))
+arr.insert(0, 0)  # 1-based indexing
 
-# Please write your code here.
-
-# Compare and sort
-def heapify(arr: List[int], n: int, i: int) -> None:
+def heapify(n: int, i: int) -> None:
     largest = i
     left = 2 * i
     right = 2 * i + 1
 
-    if left < n and arr[left] > arr[largest]:
+    if left <= n and arr[left] > arr[largest]:
         largest = left
-    
-    if right < n and arr[right] > arr[largest]:
+    if right <= n and arr[right] > arr[largest]:
         largest = right
-    
+
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
-        heapify(arr, n, largest)
+        heapify(n, largest)
 
+def heap_sort() -> None:
+    for i in range(n // 2, 0, -1):
+        heapify(n, i)
 
-def heap_sort(arr: List[int]) -> List[int]:
-    for i in range(n//2 - 1, -1, -1):
-        heapify(arr, n, i)
-    
-    for i in range(n - 1, 0, -1):
-        arr[0], arr[i] = arr[i], arr[0]
-        heapify(arr, i, 0)
-    
-    return arr
+    for i in range(n, 1, -1):
+        arr[i], arr[1] = arr[1], arr[i]
+        heapify(i - 1, 1)
 
-sorted_arr = heap_sort(arr)
-print(' '.join(map(str, sorted_arr)))
+def solution() -> str:
+    heap_sort()
+    return ' '.join(map(str, arr[1:]))
+
+print(solution())
