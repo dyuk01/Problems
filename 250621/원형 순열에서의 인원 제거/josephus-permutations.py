8ee5e3fbf1b83f8e686d0f2f34f3a6f8450b71c3
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-06-21 12:21:15 +0900

    Title: 원형 순열에서의 인원 제거, Time: 121ms, Memory: 29MB, Status: Passed - Codetree

--- 250621/원형 순열에서의 인원 제거/josephus-permutations.py
+++ 250621/원형 순열에서의 인원 제거/josephus-permutations.py
@@ -1,36 +1,55 @@
+# from typing import List
+
+# n, k = map(int, input().split())
+
+# class Queue:
+#     def __init__(self):
+#         self.queue = []
+
+#     def empty(self) -> bool:
+#         return not self.queue
+
+#     def size(self) -> int:
+#         return len(self.queue)
+
+#     def push(self, val) -> None:
+#         self.queue.append(val)
+
+#     def pop(self) -> int:
+#         return self.queue.pop(0) if not self.empty() else 0
+
+#     def front(self) -> int:
+#         return self.queue[0] if not self.empty() else 0
+
+# def solution() -> List[int]:
+#     queue = Queue()
+#     for i in range(1, n + 1):
+#         queue.push(i)
+
+#     res = []
+#     while not queue.empty():
+#         for _ in range(k - 1): 
+#             queue.push(queue.pop())
+#         res.append(queue.pop())
+
+#     return res
+
+# print(' '.join(map(str, solution())))
+
+
 from typing import List
+from collections import deque
 
 n, k = map(int, input().split())
 
-class Queue:
-    def __init__(self):
-        self.queue = []
-
-    def empty(self) -> bool:
-        return not self.queue
-
-    def size(self) -> int:
-        return len(self.queue)
-
-    def push(self, val) -> None:
-        self.queue.append(val)
-
-    def pop(self) -> int:
-        return self.queue.pop(0) if not self.empty() else 0
-
-    def front(self) -> int:
-        return self.queue[0] if not self.empty() else 0
-
 def solution() -> List[int]:
-    queue = Queue()
-    for i in range(1, n + 1):
-        queue.push(i)
-
+    queue = deque(range(1, n + 1))
     res = []
-    while not queue.empty():
-        for _ in range(k - 1): 
-            queue.push(queue.pop())
-        res.append(queue.pop())
+
+    while queue:
+        for _ in range(k - 1):
+            queue.append(queue.popleft())
+        res.append(queue.popleft())
 
     return res
 
