283d44036124efe91808c7969d778a6efd4ff265
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-06-23 15:58:44 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 79ms, Memory: 19MB, Status: Passed - Codetree

--- 250623/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 250623/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -2,37 +2,51 @@ from typing import List
 
 arr = input()
 
-# Please write your code here.
-class Stack:
+# # Please write your code here.
+# class Stack:
 
-    stack: List[str]
+#     stack: List[str]
 
-    def __init__(self):
-        self.stack = []
+#     def __init__(self):
+#         self.stack = []
     
 
-    def empty(self) -> bool:
-        return not self.stack
+#     def empty(self) -> bool:
+#         return not self.stack
     
 
-    def push(self, val) -> None:
-        self.stack.append(val)
+#     def push(self, val) -> None:
+#         self.stack.append(val)
     
 
-    def pop(self) -> None:
-        self.stack.pop(-1)
+#     def pop(self) -> None:
+#         self.stack.pop(-1)
     
+
+# def solution() -> str:
+#     stack = Stack()
+#     for s in arr:
+#         if s == "(":
+#             stack.push("(")
+#         elif s == ")":
+#             if stack.empty():
+#                 return "No"
+#             stack.pop()
+    
+#     return "Yes" if stack.empty() else "No"
+
+# print(solution())
 
 def solution() -> str:
-    stack = Stack()
+    res = []
     for s in arr:
         if s == "(":
-            stack.push("(")
+            res.append("(")
         elif s == ")":
-            if stack.empty():
+            if len(res) == 0:
                 return "No"
-            stack.pop()
+            res.pop(-1)
     
-    return "Yes" if stack.empty() else "No"
+    return "Yes" if len(res) == 0 else "No"
 
 print(solution())
