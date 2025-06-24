dede7cf646979ef718c65d9267a4e271286a1760
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-06-24 23:36:22 +0900

    Title: 수열 조작, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 250624/수열 조작/sequence-manipulation.py
+++ 250624/수열 조작/sequence-manipulation.py
@@ -1,14 +1,27 @@
 from collections import deque
+import math
 
 n = int(input())
 
 # Please write your code here.
 
+# def init_dq() -> deque:
+#     return deque([x for x in range(1, n+1)])
+
+
+# def solution(dq: deque) -> int:
+#     while len(dq) > 1:
+#         dq.popleft()
+#         dq.rotate(-1)
+
+#     return dq.pop()
+
+# print(solution(init_dq()))
+
 def solution() -> int:
-    dq = deque([x for x in range(1, n+1)])
-    while len(dq) > 1:
-        dq.popleft()
-        dq.rotate(-1)
-    return dq.pop()
+    if n < 3:
+        return n
+    return 2 * (n - 2 ** (n.bit_length() - 1))
 
 print(solution())
+# f(n) = 2(n - 2^(n.bit_length)
