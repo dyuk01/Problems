d9fd92a8e5a6d331f02ac470a5fe75db7a83c484
Peter Yuk <50248737+dyuk01@users.noreply.github.com>
2025-06-24 22:37:02 +0900

    Title: 원형 순열에서의 인원 제거, Time: 111ms, Memory: 25MB, Status: Passed - Codetree

--- 250624/원형 순열에서의 인원 제거/josephus-permutations.py
+++ 250624/원형 순열에서의 인원 제거/josephus-permutations.py
@@ -1,24 +1,43 @@
+# from collections import deque
+# from typing import List
+
+# n, k = map(int, input().split())
+
+
+# # Please write your code here.
+# # Removes k - 1'th push element
+
+# dq = deque([x for x in range(1, n+1)])
+
+# def solution() -> List[int]:
+#     res = []
+#     while len(dq) > 0:
+#         dq.rotate(1 - k)
+#         res.append(dq.popleft())
+
+#     return res
+
+# print(' '.join(map(str, solution())))
+
 from collections import deque
 from typing import List
 
 n, k = map(int, input().split())
 
+def set_deque() -> deque:
+    dq = deque()
+    for i in range(1, n+1):
+        dq.append(i)
+    return dq
 
-# Please write your code here.
-# Removes k - 1'th push element
+def pop_k_repeat(dq: deque) -> List[int]:
+    result = []
+    while len(dq) > 1:
+        dq.rotate(-k+1)
+        result.append(dq.popleft())
+    return result + [dq[0]]
 
-dq = deque([x for x in range(1, n+1)])
+def solution() -> str:
+    return ' '.join(map(str, pop_k_repeat(set_deque())))
 
-def solution() -> List[int]:
-    res = []
-    while len(dq) > 0:
-        # for _ in range(k-1):
-        #     dq.append(dq.popleft())
-        # res.append(dq.popleft())
-        dq.rotate(1 - k)
-        res.append(dq.popleft())
-    # res.append(dq.popleft())
-    
-    return res
-
-print(' '.join(map(str, solution())))
+print(solution())
